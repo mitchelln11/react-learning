a9970abd011cefab5a225469794e0ea22491d540
Mitchell Naleid <mitchellnaleid@gmail.com>
2020-02-24 12:37:55 -0600

    added comments to help display what's going on

--- like_button.js
+++ like_button.js
@@ -2,24 +2,28 @@
 
 const e = React.createElement; // Any reference to this will be used with e
 
+// ---- Begin class -----
 class LikeButton extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { liked: false };
+        this.state = { liked: false }; // Begin button with a false value
     }
 
     render() {
+        // If the liked value changes to true, add text stating that you liked it
         if (this.state.liked) {
             return 'You liked this.';
         }
 
-        return e(
-            'button',
-            { onClick: () => this.setState({ liked: true }) },
-            'Like'
+        // Actual effect of clicking on the button
+        return e( // e could also be shown as React.createElement, but we added it as a const above ^^ to make it quicker
+            'button', // What are we effecting?
+            { onClick: () => this.setState({ liked: true }) }, // What are we doing with the button? (Clicking, then changing value to true)
+            'Like' 
         );
     }
 }
+// ---- End class ----- 
 
-const domContainer = document.querySelector('#like_button_container');
+const domContainer = document.querySelector('#like_button_container'); // Shorten this by setting const to domContainer, then use domContainer here on out
 ReactDOM.render(e(LikeButton), domContainer);
